f2f1a113789ce0d468c266a4563522d0c3b542be
Sreyas-R <sreyasrejil1@gmail.com>
2022-08-11 22:12:18 +0530

    C code to print a message using Threads

---
+++ thrdmsg.c
@@ -0,0 +1,25 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void *printLab(void *args)
+{
+    printf("LAB");
+    pthread_exit(NULL);
+}
+void *printPDC(void *args)
+{
+   printf("PDC ");
+    pthread_exit(NULL);
+}
+int main()
+{
+    pthread_t thread1;
+    pthread_t thread2;
+    
+    pthread_create(&thread1, 0, &printPDC, NULL);
+    pthread_create(&thread2, 0, &printLab, NULL);
+    pthread_join(thread1, 0);
+    
+    return  0;
+}
